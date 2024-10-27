4cf623c5773b70ca21fe27a3aba8c82b1519d901
Romulo Franco <rfsvm25@gmail.com>
2024-10-27 08:48:00 -0300

    creating spring boot project

---
+++ src/main/java/br/com/romulofranc0/reserva_hotel/ReservaHotelApplication.java
@@ -0,0 +1,13 @@
+package br.com.romulofranc0.reserva_hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/romulofranc0/reserva_hotel/ReservaHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.romulofranc0.reserva_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
